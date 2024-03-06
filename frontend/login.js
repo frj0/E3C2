5b0fcd0b00a7d1f9de6cde1ecb04a0c833ca6d44
frj0 <touskekunn@gmail.com>
2024-03-07 06:04:02 +0900

    problema js

--- frontend/login.js
+++ frontend/login.js
@@ -3,7 +3,7 @@ function loguear ()
 
     let user=document.getElementById("usuario").value;
     let pass=document.getElementById("contrasena").value;
-if(user=="usuario1" && pass=="password")
+if(user=="Usuario1" && pass=="password")
 {
     window.location="index.html";
 
