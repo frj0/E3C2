eca355a165358e08c84d8a38237da605048bf0f3
frj0 <touskekunn@gmail.com>
2024-03-19 07:23:13 +0900

    href

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -5,7 +5,7 @@ function loguear() {
     let pass = document.getElementById("contrasena").value;
     
     if (user == "Usuario1" && pass == "password") {
-        window.location.replace("../frontend/index.html");
+        window.location.href("../frontend/index.html");
     } else {
         alert("Datos incorrectos");
     }
