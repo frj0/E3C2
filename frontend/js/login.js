ab32c3eb72a5b14b2e8a22dde2d0f85b32d0ae9d
frj0 <touskekunn@gmail.com>
2024-03-19 07:51:04 +0900

    index

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -5,7 +5,7 @@ function loguear() {
     let pass = document.getElementById("contrasena").value;
     
     if (user == "Usuario1" && pass == "password") {
-        window.location.href("../frontend/index.html");
+        window.location.href("../index.html");
     } else {
         alert("Datos incorrectos");
     }
